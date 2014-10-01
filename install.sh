5f72fdb24ff97b5d5055d5bd0795c7aff4143bbd
Md. Minhazul Haque <mdminhazulhaque@gmail.com>
2014-10-01 14:26:31 +0600

    first release

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Checking for administrator privileges"
+sudo -v
+echo "Checking for Unity environment"
+if [[ -d /usr/share/unity/icons ]]
+then
+    echo "Backing up launcher_bfb.png"
+    mv /usr/share/unity/icons/launcher_bfb.png /usr/share/unity/icons/launcher_bfb.png.bak
+    echo "Downloading launcher_bfb.png"
+    wget -qO usr/share/unity/icons/launcher_bfb.png https://github.com/Raihan-Ali/dashicon-ubuntubd/raw/master/launcher_bfb.png
+    echo "Exiting successfully"
+    exit 0
+else
+    echo "/usr/share/unity/icons does not exist"
+    echo "Exiting unsuccessfully"
+    exit 1
+fi
